0fec4f97850911d3019efff0301f9090000a7a62
victorcabeceira <victorfgcabeceira@gmail.com>
2014-04-12 14:33:13 -0300

    Comando para criar uma aplicaao : rails new todeolho

---
+++ todeolho/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todeolho::Application.config.secret_key_base = 'c5df5748558fa76a6acb2a319d58f5e869316d8a68d2fb8b04b708323ede1b6a224d40947d28394601e84a39225772ac53147d840e317112b911668afd90c1b2'
---
+++ todeolho/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Todeolho::Application.config.session_store :cookie_store, key: '_todeolho_session'
